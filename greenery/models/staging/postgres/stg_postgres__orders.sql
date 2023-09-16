09ec0da4627752435b88345dd61168c72c57e0c5
Berkay Kocak <kocakberkayy@gmail.com>
2023-09-16 15:34:25 +0000

    orders are edited

--- greenery/models/staging/postgres/stg_postgres__orders.sql
+++ greenery/models/staging/postgres/stg_postgres__orders.sql
@@ -7,17 +7,17 @@ with source as(
 , renamed_recast as(
     select
     address_id
-    , created_at
-    , delivered_at
-    , estimated_delivery_at
+    , created_at AS order_created_at
+    , delivered_at AS order_delivered_at
+    , estimated_delivery_at AS order_estimated_delivery_at
     , order_cost
     , order_id
     , order_total
     , promo_id
-    , shipping_cost
-    , shipping_service
-    , status
-    , tracking_id
+    , shipping_cost AS order_shipping_cost
+    , shipping_service AS order_shipping_service
+    , status AS order_status
+    , tracking_id AS order_tracking_id
     , user_id
     from source
 )
