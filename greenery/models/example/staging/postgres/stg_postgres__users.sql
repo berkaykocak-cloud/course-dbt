eb5e88e9a4e847b2aeb4e64d06ac17a57dfa1a0b
Berkay Kocak <kocakberkayy@gmail.com>
2023-09-16 08:32:12 +0000

    first staging added

---
+++ greenery/models/example/staging/postgres/stg_postgres__users.sql
@@ -0,0 +1,20 @@
+{{config(materialized='view')}}
+
+with source as(
+    select * from {{source('postgres','users')}}
+)
+
+, renamed_recast as(
+    select
+    user_id
+    , first_name
+    , last_name
+    , email
+    , phone_number
+    , created_at
+    , updated_at
+    , address_id
+    from source
+)
+
+SELECT * FROM renamed_recast
