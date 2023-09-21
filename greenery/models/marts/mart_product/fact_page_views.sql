50cfd56beab2cc049f06f152250544967431844b
Berkay Kocak <kocakberkayy@gmail.com>
2023-09-21 11:37:07 +0000

    updated fact_page_views

--- greenery/models/marts/mart_product/fact_page_views.sql
+++ greenery/models/marts/mart_product/fact_page_views.sql
@@ -12,7 +12,7 @@ event_day
 FROM {{ref('int_product_events')}}
 )
 
-,  product_details AS(
+, product_details AS(
 
 SELECT
     product_id
@@ -21,5 +21,15 @@ SELECT
 FROM {{ref('stg_postgres__products')}}
 )
 
-
 SELECT
+product_events.event_day
+, product_details.product_name
+, product_details.product_price
+, product_events.product_id
+, product_events.add_to_carts
+, product_events.checkouts
+, product_events.package_shippeds
+, product_events.page_views
+, product_events.product_daily_cvr
+FROM product_events
+LEFT JOIN product_details ON product_events.product_id= product_details.product_id
