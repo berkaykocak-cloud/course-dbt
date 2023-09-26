5ec1d9dde38a5462887f771309bda88c914d5574
Berkay Kocak <kocakberkayy@gmail.com>
2023-09-26 08:41:27 +0000

    some more styling fixes

--- greenery/models/intermediate/int_product_events.sql
+++ greenery/models/intermediate/int_product_events.sql
@@ -2,21 +2,21 @@
 
 WITH aggregation AS(
 SELECT 
-    DATE_TRUNC('day',event_created_at) AS event_day
-    , product_id
-    , SUM(CASE WHEN event_type ='add_to_cart' THEN 1 ELSE 0 END) AS add_to_carts
-    , SUM(CASE WHEN event_type ='checkout' THEN 1 ELSE 0 END) AS checkouts
-    , SUM(CASE WHEN event_type ='package_shipped' THEN 1 ELSE 0 END) AS package_shippeds
-    , SUM(CASE WHEN event_type ='page_view' THEN 1 ELSE 0 END) AS page_views
+  DATE_TRUNC('day',event_created_at) AS event_day
+  , product_id
+  , SUM(CASE WHEN event_type ='add_to_cart' THEN 1 ELSE 0 END) AS add_to_carts
+  , SUM(CASE WHEN event_type ='checkout' THEN 1 ELSE 0 END) AS checkouts
+  , SUM(CASE WHEN event_type ='package_shipped' THEN 1 ELSE 0 END) AS package_shippeds
+  , SUM(CASE WHEN event_type ='page_view' THEN 1 ELSE 0 END) AS page_views
 FROM {{ref('stg_postgres__events')}}
 GROUP BY 1,2 )
 
 SELECT 
-event_day
-, product_id
-, add_to_carts
-, checkouts
-, package_shippeds
-, page_views
-, div0(checkouts,page_views) AS product_daily_cvr
+  event_day
+  , product_id
+  , add_to_carts
+  , checkouts
+  , package_shippeds
+  , page_views
+  , div0(checkouts,page_views) AS product_daily_cvr
 FROM aggregation
--- greenery/models/marts/mart_product/fact_product_events.sql
+++ greenery/models/marts/mart_product/fact_product_events.sql
@@ -2,34 +2,33 @@
 
 WITH product_events AS(
 SELECT 
-event_day
-, product_id
-, add_to_carts
-, checkouts
-, package_shippeds
-, page_views
-, product_daily_cvr
+  event_day
+  , product_id
+  , add_to_carts
+  , checkouts
+  , package_shippeds
+  , page_views
+  , product_daily_cvr
 FROM {{ref('int_product_events')}}
 )
 
 , product_details AS(
-
 SELECT
-    product_id
-    , product_name
-    , product_price
+  product_id
+  , product_name
+  , product_price
 FROM {{ref('stg_postgres__products')}}
 )
 
 SELECT
-product_events.event_day
-, product_details.product_name
-, product_details.product_price
-, product_events.product_id
-, product_events.add_to_carts
-, product_events.checkouts
-, product_events.package_shippeds
-, product_events.page_views
-, product_events.product_daily_cvr
+  product_events.event_day
+  , product_details.product_name
+  , product_details.product_price
+  , product_events.product_id
+  , product_events.add_to_carts
+  , product_events.checkouts
+  , product_events.package_shippeds
+  , product_events.page_views
+  , product_events.product_daily_cvr
 FROM product_events
 LEFT JOIN product_details ON product_events.product_id= product_details.product_id
